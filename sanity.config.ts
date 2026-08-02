3d73c35da0a9a9ce11e52a560e9cf745c721e7c6
Saif Ali <mesaifalee@gmail.com>
2026-08-02 16:42:36 +0500

    Add redesigned Sanity Studio theme, structure layout, and schema updates

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,15 +2,22 @@ import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import { schema } from './schemaTypes'
+import { structure } from './structure'
+import { monochromeTheme } from './theme'
 
 export default defineConfig({
   name: 'default',
-  title: 'Hala CMS',
+  title: 'Hala Smart Technologies Studio',
 
   projectId: 'gmjjzklq',
   dataset: 'production12',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [
+    structureTool({ structure }),
+    visionTool(),
+  ],
 
   schema: schema,
+
+  theme: monochromeTheme,
 })
